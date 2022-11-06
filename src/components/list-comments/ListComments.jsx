86dee229335bba6f808baa2a2d96f70d1f114abf
filimonovSB <fsb2603@mail.ru>
2022-11-06 21:37:07 +0700

    create ListComments component

---
+++ src/components/list-comments/ListComments.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Card } from 'antd'
+import styled from 'styled-components'
+import Box from '../UI/Box'
+
+const ListComments = ({comments}) => {
+
+    const FlexWrapper = styled.div`
+        display: flex;
+        flex-wrap: wrap;
+    `
+    return (
+        <Box mt={15} mb={15}>
+            <FlexWrapper>
+                {comments.length 
+                    ? 
+                        comments.map((comment) => (
+                                <Card
+                                    key={comment.id}
+                                    title="Комментарий"
+                                    bordered={false}
+                                    style={{ width: '350px', margin: '10px' }}
+                                >
+                                    <h4>Название:</h4> {comment.name}
+                                    <h4>email:</h4> {comment.email}
+                                    <h4>Содержание:</h4> {comment.body}
+                                    <br />
+                                </Card>
+                            )
+                        )
+                    : 
+                    <h2>Коментариев к этому посту нет!</h2>
+                }
+        </FlexWrapper>      
+    </Box>
+    )
+}
+
+export default ListComments
--- src/pages/CommentsPage.jsx
+++ src/pages/CommentsPage.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { Card, Button } from 'antd'
+import {  Button } from 'antd'
 import { ArrowLeftOutlined } from '@ant-design/icons'
 
 import Box from '../components/UI/Box'
 import { loadComments } from '../store/thunks/loadComments'
-
+import ListComments from '../components/list-comments/ListComments'
 
 const CommentsPage = () => {
   const navigate = useNavigate()
@@ -32,33 +32,8 @@ const CommentsPage = () => {
         />
         Вернуться к постам
       </Box>
-      <Box mt={15} mb={15}>
-        <h1>Пост {id}</h1>
-        <div
-          style={{
-            display: 'flex',
-            flexWrap: 'wrap',
-          }}
-        >
-          {comments.length ? (
-            comments.map((comment) => (
-              <Card
-                key={comment.id}
-                title="Комментарий"
-                bordered={false}
-                style={{ width: '350px', margin: '10px' }}
-              >
-                <h4>Название:</h4> {comment.name}
-                <h4>email:</h4> {comment.email}
-                <h4>Содержание:</h4> {comment.body}
-                <br />
-              </Card>
-            ))
-          ) : (
-            <h2>Коментариев к этому посту нет!</h2>
-          )}
-        </div>
-      </Box>
+      <h1>Пост {id}</h1>
+      <ListComments comments={comments}/>
     </>
   )
 }
